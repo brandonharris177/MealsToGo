50a1eb43257c69e64d8c084f3acc772ecaf722af
Brandon Harris <brandonharris177@gmail.com>
2021-06-16 14:59:23 -0600

    added seperate styling sheet for RestaurantCards

---
+++ src/features/restaurants/components/RestaurantCardStyling.js
@@ -0,0 +1,34 @@
+import styled from "styled-components/native";
+import { Card } from "react-native-paper";
+import { Image, View } from "react-native";
+
+export const StyledCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+export const CardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+export const RatingOpenWrapper = styled(View)`
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const RatingWrapper = styled(View)`
+  flex-direction: row;
+  padding-bottom: ${(props) => props.theme.space[2]};
+  justify-content: space-between;
+`;
+
+export const IconWrapper = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TypeIcon = styled(Image)`
+  width: ${(props) => props.theme.sizes[1]};
+  height: ${(props) => props.theme.sizes[1]};
+  margin-left: ${(props) => props.theme.space[2]};
+`;
