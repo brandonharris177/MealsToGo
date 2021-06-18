5f80484d86f137ea8234f17f28eefc189be928dd
Brandon Harris <brandonharris177@gmail.com>
2021-06-18 12:50:07 -0600

    retreiveRestaurants working and showing correct number of cards

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,13 +1,42 @@
 import React, { useState, createContext, useEffect, useMemo } from "react";
-import { restaurantRequest, restaurantTransform } from "./Restaurants.service";
+import {
+  restaurantsRequest,
+  restaurantsTransform,
+} from "./Restaurants.service";
 
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const retrieveRestaurants = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      restaurantsRequest()
+        .then(restaurantsTransform)
+        .then((res) => {
+          setIsLoading(false);
+          setRestaurants(res);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err);
+        });
+    }, 2000);
+  };
+
+  useEffect(() => {
+    retrieveRestaurants();
+  }, [restaurants]);
+
   return (
     <RestaurantsContext.Provider
       value={{
-        restaurants: [1, 2, 3, 4, 5, 6, 7],
+        restaurants: restaurants,
+        isLoading: isLoading,
+        error: error,
       }}
     >
       {children}
