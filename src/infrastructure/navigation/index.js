b6e69099c39282295b74cfc7cba9a9fe49fd0a98
Brandon Harris <brandonharris177@gmail.com>
2021-07-13 12:08:00 -0600

    added restaurants.navigation.js to navigation and fixed headers

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
-import { RestaurantsScreen } from "../../features/restaurants/screens/Restaurants.screen";
+import { RestaurantsNavigator } from "./restaurants.navigator";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import { tabBarOptions } from "./Navigation.styling";
+import { tabBarOptions } from "./navigation.index.styling";
 
 const Tab = createBottomTabNavigator();
 
@@ -59,7 +59,7 @@ export const Navigation = () => {
         screenOptions={CreateScreenOptions}
         tabBarOptions={tabBarOptions}
       >
-        <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+        <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
         <Tab.Screen name="Map" component={Map} />
         <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
